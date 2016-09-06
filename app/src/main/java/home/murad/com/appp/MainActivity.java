358f4ecaef3d9460bea5d6d6642c1a3674d36e5f
murad shah <murad.hadi@gmail.com>
2016-09-07 04:22:50 +0500

    Thiis is my Commit message

--- app/src/main/java/home/murad/com/appp/MainActivity.java
+++ app/src/main/java/home/murad/com/appp/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends ActionBarActivity {
     }*/
     private void refreshScreen(){
 
-    //Call to server to grab list of student records. this is a asynkljljkljk
+    //Call to server to grab list of student records. this is a asynk
     //restService.getInstituteService().getStudent(new Callback<List<Student>>()
     // restService.getCategoryService().getCategory(new Callback<List<Category>>() {
     // });
